9be2f8994da90ba07964c87dd4fedd776c9c6d40
BettyDetroit <caubelnelly@gmail.com>
2023-01-29 14:04:22 +0100

    Quete express 8 - JWT sans le bonus

--- app.js
+++ app.js
@@ -8,30 +8,54 @@ app.use(express.json());
 
 const port = process.env.APP_PORT ?? 5000;
 
+const userHandlers = require("./userHandlers");
+const movieHandlers = require("./movieHandlers");
+const { hashPassword, verifyPassword, verifyToken } = require("./auth");
+
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
 
+// const isItStehpanie = (req, res) => {
+//   if (req.body.email === "stephanie.demonaco@example.com" && req.body.password === "monnouveaupassword") {
+//     res.send("Credentials are valid");
+//   } else {
+//     res.sendStatus(401);
+//   }
+// };
+
+// app.post("/api/login", isItStephanie)
+
 app.get("/", welcome);
 
-const movieHandlers = require("./movieHandlers");
+////// Public routes
+app.post(
+  "/api/login",
+  userHandlers.getUserByEmailWithPasswordAndPassToNext,
+  verifyPassword
+); ////// /!\ Login is a public route
+
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+
+app.post("/api/users", hashPassword, userHandlers.postUser);
+//////
+
+////// Routes to protect
+app.use(verifyToken);
+
 app.post("/api/movies", movieHandlers.postMovie);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
-const userHandlers = require("./userHandlers");
-const { hashPassword } = require("./auth.js");
-
-app.get("/api/users", userHandlers.getUsers);
-app.get("/api/users/:id", userHandlers.getUserById);
-app.post("/api/users", hashPassword, userHandlers.postUser);
 app.put("/api/users/:id", hashPassword, userHandlers.updateUser);
-// app.post("/api/users", hashPassword, userHandlers.postUser);
-// app.put("/api/users/:id", hashPassword, userHandlers.updateUser);
 app.delete("/api/users/:id", userHandlers.deleteUser);
+
+//////
 
 app.listen(port, (err) => {
   if (err) {
--- userHandlers.js
+++ userHandlers.js
@@ -1,8 +1,9 @@
 const database = require("./database");
 
 const getUsers = (req, res) => {
-  const initialSql = "select id, firstname, lastname, email, city, language from users";
+  const initialSql = "select * from users";
   const where = [];
+  //id, firstname, lastname, email, city, language
 
   if (req.query.city != null) {
     where.push({
@@ -41,7 +42,8 @@ const getUserById = (req, res) => {
   const id = parseInt(req.params.id);
 
   database
-    .query("select id, firstname, lastname, email, city, language from users where id = ?", [id])
+    .query("select * from users where id = ?", [id])
+    //id, firstname, lastname, email, city, language
     .then(([users]) => {
       if (users[0] != null) {
         res.json(users[0]);
@@ -75,19 +77,19 @@ const postUser = (req, res) => {
 
 const updateUser = (req, res) => {
   const id = parseInt(req.params.id);
-  const { firstname, lastname, email, city, language, hashedPassword } = req.body;
+  const { firstname, lastname, email, city, language } = req.body;
 
   database
     .query(
-      "update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ? hashedPassword = ?,where id = ?",
-      [firstname, lastname, email, city, language, hashedPassword, id]
+      "update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?",
+      [firstname, lastname, email, city, language, id]
     )
     .then(([result]) => {
       if (result.affectedRows === 0) {
         res.status(404).send("Not Found");
       } else {
         res.sendStatus(204);
-      }
+      }  
     })
     .catch((err) => {
       console.error(err);
@@ -113,10 +115,37 @@ const deleteUser = (req, res) => {
     });
 };
 
+
+const getUserByEmailWithPasswordAndPassToNext = (req, res, next) => {
+  const { email } = req.body;
+
+  database
+    .query("select * from users where email = ?", [email])
+    .then(([users]) => {
+      if (users[0] != null) {
+        req.user = users[0];
+
+        next();
+      } else {
+        res.sendStatus(401);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+
 module.exports = {
   getUsers,
   getUserById,
   postUser,
   updateUser,
   deleteUser,
+  getUserByEmailWithPasswordAndPassToNext,
 };
+
+ // if (req.payload.sub !== req.body.id) {
+      //   res.status(403).send("Forbidden");
+      // }
